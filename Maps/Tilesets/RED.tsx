3396d4976fa08c474b0876f0c7328c0d4e40fcad
Gusp3h <gattohmatto666@gmail.com>
2020-03-11 18:43:50 +0100

    All Resources Added

---
+++ Maps/Tilesets/RED.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="RED" tilewidth="16" tileheight="16" tilecount="1" columns="1">
+ <image source="../../Shitty Assets/RED.png" width="16" height="16"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="15.9565" height="16.087"/>
+  </objectgroup>
+ </tile>
+</tileset>
