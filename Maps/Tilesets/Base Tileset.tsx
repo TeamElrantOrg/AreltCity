034be0e90481ea7eb3caa3b73a6f66ee2014c1aa
Gusp3h <gattohmatto666@gmail.com>
2020-03-18 18:50:13 +0100

    Better Maps

--- Maps/Tilesets/Base Tileset.tsx
+++ Maps/Tilesets/Base Tileset.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.3.3" name="Base Tileset" tilewidth="16" tileheight="16" spacing="1" tilecount="1767" columns="57">
- <image source="../../Project44/Assets/Art/roguelikeSheet_transparent.png" width="968" height="526"/>
+ <image source="../../Assets/Art/roguelikeSheet_transparent.png" width="968" height="526"/>
  <terraintypes>
   <terrain name="Dirt" tile="578"/>
   <terrain name="Gravel" tile="0"/>
