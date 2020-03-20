e469b02d3dff95f9e21f27b06bde8e24b93cfe1e
Team Elrant <62069503+ThatTeamElrant@users.noreply.github.com>
2020-03-20 14:44:44 +0100

    ho ffatto
    si

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,13 +7,19 @@ public class CameraController : MonoBehaviour
     public GameObject followTarget;
     private Vector3 targetPos;
     public float moveSpeed;
-
     public static bool cameraExists;
-
+     Vector2 viewPortSize;
+     Camera cam;
+    public float viewPortFactor;
+    private Vector3 currentVelocity;
+    public float followDuration;
+    public float maximumFollowSpeed;
+    public Transform player;
+    Vector3 targetPosition;
     // Start is called before the first frame update
     void Start()
     {
-
+        cam = Camera.main;
        
 
         if (!cameraExists)
@@ -34,5 +40,17 @@ public class CameraController : MonoBehaviour
     {
         targetPos = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z);
         transform.position = Vector3.Lerp (transform.position, targetPos, moveSpeed * Time.deltaTime);
+        //viewPortSize = cam.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height)) - cam.ScreenToWorldPoint(Vector2.zero) * viewPortFactor;
+        //targetPosition = player.position - new Vector3(0, 0, 10);
+        //transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref currentVelocity, followDuration, maximumFollowSpeed);
+    }
+    void OnDrawGizmos()
+    {
+        Color c = Color.red;
+        c.a = 0.3f;
+        Gizmos.DrawCube(transform.position, viewPortSize);
+
+
+
     }
 }
