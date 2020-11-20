11a6f4469d618f5c4237c2f45929e709eaf76805
Gusp3h <Gusp3h@gmail.com>
2020-11-20 08:09:01 +0100

    Commented on PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,30 +10,33 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D body;
     public Animator animator;
     private bool moving;
- 
- 
+    //Joystick for eventual mobile controls.
+    // public joystick joystick;
     // Use this for initialization
     void Start () {
         body = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         moving = false;
-        // isAttacking = false;
+        // attacking = false;
     }
     
     // Update is called once per frame
     void FixedUpdate () {
  
-        // Attack Test
-        if(Input.GetButtonDown("Fire1"))
-        {
-            animator.SetTrigger("Attack");
-        }
- 
-        // Get input
+        // We'll use this code down here for attacks later on,
+        // Works on gamepads too.
+        //if(Input.GetButtonDown("Fire1"))
+        // {
+        //      animator.SetTrigger("Attack");
+        // }
+        // Get input using .GetAxisRaw
+        // Can be replaced by 
+        // Joystick.Horizontal or Joystick.Vertical
+
         xInput = Input.GetAxisRaw("Horizontal");
         yInput = Input.GetAxisRaw("Vertical");
  
-        // If moving, Move with 3d Vector
+        // If moving(Player gives Input), Then move with 3D Vector
         moving = (xInput != 0 || yInput != 0);
  
         if (moving)
@@ -42,17 +45,11 @@ public class PlayerMovement : MonoBehaviour
  
             // BAD MOVEMENT - Circumvents RigidBody2D Physics
             // transform.position += moveVector * speed * Time.deltaTime;
-            //
- 
             // CORRECT MOVEMENT
-            body.MovePosition(new Vector2((transform.position.x + moveVector.x * speed * Time.deltaTime),
-                   transform.position.y + moveVector.y * speed * Time.deltaTime));
- 
- 
+            body.MovePosition(new Vector2((transform.position.x + moveVector.x * speed * Time.deltaTime), transform.position.y + moveVector.y * speed * Time.deltaTime));
             animator.SetFloat("xInput", xInput);
             animator.SetFloat("yInput", yInput);
         }
- 
         animator.SetBool("moving", moving);
     }
 }
