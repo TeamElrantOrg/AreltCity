4303c02406bd455c2c07e38da3e893da42692402
Gusp3h <Gusp3h@gmail.com>
2020-11-20 22:10:23 +0100

    Smoothed and Bounded Camera (comments pending)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
     private float xInput, yInput;
     public int speed;
     private Rigidbody2D body;
     public Animator animator;
     private bool moving;
+
     //Joystick for eventual mobile controls.
     // public joystick joystick;
     // Use this for initialization
+    
     void Start () {
         body = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
@@ -22,7 +23,7 @@ public class PlayerMovement : MonoBehaviour
     
     // Update is called once per frame
     void FixedUpdate () {
- 
+
         // We'll use this code down here for attacks later on,
         // Works on gamepads too.
         //if(Input.GetButtonDown("Fire1"))
@@ -32,17 +33,14 @@ public class PlayerMovement : MonoBehaviour
         // Get input using .GetAxisRaw
         // Can be replaced by 
         // Joystick.Horizontal or Joystick.Vertical
-
         xInput = Input.GetAxisRaw("Horizontal");
         yInput = Input.GetAxisRaw("Vertical");
- 
         // If moving(Player gives Input), Then move with 3D Vector
         moving = (xInput != 0 || yInput != 0);
  
         if (moving)
         {
             var moveVector = new Vector3(xInput , yInput, 0);   
- 
             // BAD MOVEMENT - Circumvents RigidBody2D Physics
             // transform.position += moveVector * speed * Time.deltaTime;
             // CORRECT MOVEMENT
