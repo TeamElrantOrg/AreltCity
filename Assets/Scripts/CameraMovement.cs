92d4825183512087bbeef11cd9c28e23dfb19659
Gusp3h <Gusp3h@gmail.com>
2020-11-21 13:46:54 +0100

    Added Mobile support, Commented heavily.

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    // The Camera's transform position
     public Transform target;
+
+    // Smoothing amount for Lerp
     public float smoothing;
+    
+    // Max and Min position (Boundaries)
     public Vector2 maxPosition;
     public Vector2 minPosition;
     
@@ -18,12 +23,18 @@ public class CameraMovement : MonoBehaviour
 
     // Update is called once per frame
     void LateUpdate()
-    {
+    {   
+        // If the camera's position isn't the same as the target specified in the editor
         if (transform.position != target.position)
         {
+            // Make a Vector3 with the Target Position
             Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+            // Clamp movement inside Boundaries
             targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
             targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
+
+            // Move the camera's transform to the target position using Lerp
             transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
         }
     }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // Cross-Platform floats for Input
     private float xInput, yInput;
+
+    // Player movement speed
     public int speed;
+
     private Rigidbody2D body;
     public Animator animator;
+
+    // Variable to check if player is moving
     private bool moving;
 
-    //Joystick for eventual mobile controls.
-    // public joystick joystick;
-    // Use this for initialization
+    // Variable to change in editor, depending on build target
+    public bool mobile;
+
+    // Joystick for mobile input
+    public Joystick myJoystick;
+    
+    // public bool attacking; for attacks later on
+
     
     void Start () {
         body = GetComponent<Rigidbody2D>();
@@ -24,30 +35,29 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate () {
 
-        // We'll use this code down here for attacks later on,
-        // Works on gamepads too.
-        //if(Input.GetButtonDown("Fire1"))
-        // {
-        //      animator.SetTrigger("Attack");
-        // }
         // Get input using .GetAxisRaw
-        // Can be replaced by 
-        // Joystick.Horizontal or Joystick.Vertical
-        xInput = Input.GetAxisRaw("Horizontal");
-        yInput = Input.GetAxisRaw("Vertical");
+        // Can be replaced by Joystick.Horizontal or Joystick.Vertical if the variable Mobile is set to True in the editor
+        if(mobile == false)
+        {
+            xInput = Input.GetAxisRaw("Horizontal");
+            yInput = Input.GetAxisRaw("Vertical");
+        } else {
+            xInput = myJoystick.Horizontal;
+            yInput = myJoystick.Vertical;
+        }
         // If moving(Player gives Input), Then move with 3D Vector
         moving = (xInput != 0 || yInput != 0);
  
         if (moving)
         {
-            var moveVector = new Vector3(xInput , yInput, 0);   
-            // BAD MOVEMENT - Circumvents RigidBody2D Physics
-            // transform.position += moveVector * speed * Time.deltaTime;
-            // CORRECT MOVEMENT
+            var moveVector = new Vector3(xInput , yInput, 0);
+            // We change the Rigidbody position to a Vector2, composed of Transform, Move direction, Speed and time.deltatime
             body.MovePosition(new Vector2((transform.position.x + moveVector.x * speed * Time.deltaTime), transform.position.y + moveVector.y * speed * Time.deltaTime));
+            // We apply the Input variables to our Animator, for it to change animation
             animator.SetFloat("xInput", xInput);
             animator.SetFloat("yInput", yInput);
         }
+        // Puts the player in a moving state
         animator.SetBool("moving", moving);
     }
 }
